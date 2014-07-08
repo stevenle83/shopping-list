9d602bc272349d391c045da8d9e0344170a47d5e
Steven Le <lesteven1983@gmail.com>
2014-07-08 15:47:51 -0500

    shopping-list
    main components done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 	
+	$('.add').on('click', function() {
+		var item = $('.inputField').val();
+		$('.inputField').val('');
+		$('.listItem').append('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-danger">REMOVE</div></div>');
+			
+		$('.listItem').on('click', '.remove', function() {
+			$(this).parent('.item').remove();
+		});
 
+		$('.listItem').on('click', '.input', function () {
+			$(this).toggleClass('cross');
+		});
 
+	});
 
 });
