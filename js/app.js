b8d959f52159b6c70c1a9746339f255b1e684d8d
Steven Le <lesteven1983@gmail.com>
2014-07-09 15:00:06 -0500

    shopping-list
    slight changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-	
+
 	$('.add').on('click', function() {
 		var item = $('.inputField').val();
 		$('.inputField').val('');
 		$('.listItem').append('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>');
-			
-		$('.listItem').on('click', '.remove', function() {
+		$('.remove').on('click', function() {
 			$(this).parent('.item').remove();
 		});
 	});
-
+		
 	$('.listItem').on('click', '.input', function () {
 		$(this).toggleClass("cross");
 	});
-
+	
 });
