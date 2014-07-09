d8abeb5043f50fb0a9257cfeca42edd7d140dc6f
Steven Le <lesteven1983@gmail.com>
2014-07-09 15:16:22 -0500

    shopping-list
    added touch function for mobile

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 		var item = $('.inputField').val();
 		$('.inputField').val('');
 		$('.listItem').append('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>');
+		
 		$('.remove').on('click', function() {
 			$(this).parent('.item').remove();
 		});
@@ -12,5 +13,9 @@ $(document).ready(function() {
 	$('.listItem').on('click', '.input', function () {
 		$(this).toggleClass("cross");
 	});
-	
+
+	$('.listItem').on('tap', '.input', function () {
+		$(this).toggleClass("cross");
+	});
+
 });
