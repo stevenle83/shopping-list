2c8b334bb194750d9411ad888fcd7c94b86a7661
Steven Le <lesteven1983@gmail.com>
2014-07-09 16:29:08 -0500

    shopping-list
    final changes for touch functions

--- js/app.js
+++ js/app.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 	$('.add').on('click', function() {
 		var item = $('.inputField').val();
 		$('.inputField').val('');
-		$('.listItem').append('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>');
+		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');
 		
 		$('.remove').on('click', function() {
-			$(this).parent('.item').remove();
+			$(this).parent('.item').fadeOut('slow');
 		});
 	});
 		
@@ -14,8 +14,19 @@ $(document).ready(function() {
 		$(this).toggleClass("cross");
 	});
 
-	$('.listItem').on('tap', '.input', function () {
-		$(this).toggleClass("cross");
+
+	$('.add').on('tap', function() {
+		var item = $('.inputField').val();
+		$('.inputField').val('');
+		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');
+
+		$('.remove').on('tap', function() {
+			$(this).parent('.item').fadeOut('slow');
+		});
 	});
+	
+		$('.listItem').on('tap', '.input', function () {
+			$(this).toggleClass("cross");
+		});
 
 });
