3cd85c6d4ac84c6595bbd68bf033acc7b5c50552
Steven Le <lesteven1983@gmail.com>
2014-07-10 15:40:17 -0500

    shopping-list
    added sortable function with jquery ui

--- js/app.js
+++ js/app.js
@@ -3,30 +3,33 @@ $(document).ready(function() {
 	$('.add').on('click', function() {
 		var item = $('.inputField').val();
 		$('.inputField').val('');
-		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');
+		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');	
+	});
 		
-		$('.remove').on('click', function() {
-			$(this).parent('.item').fadeOut('slow');
-		});
+	$('.remove').on('click', function() {
+		$(this).parent('.item').fadeOut('slow');
 	});
 		
 	$('.listItem').on('click', '.input', function () {
 		$(this).toggleClass("cross");
 	});
 
+	$('.listItem').sortable();
 
+
+	//Added tap function for mobile devices
 	$('.add').on('tap', function() {
 		var item = $('.inputField').val();
 		$('.inputField').val('');
-		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');
-
-		$('.remove').on('tap', function() {
-			$(this).parent('.item').fadeOut('slow');
-		});
+		$('<div class="item"><li class="input list-group-item">' +item+ '</li><div class="remove btn btn-default">REMOVE</div></div>').appendTo('.listItem').hide().fadeIn('slow');	
 	});
 	
-		$('.listItem').on('tap', '.input', function () {
-			$(this).toggleClass("cross");
-		});
+	$('.remove').on('tap', function() {
+		$(this).parent('.item').fadeOut('slow');
+	});
+
+	$('.listItem').on('tap', '.input', function () {
+		$(this).toggleClass("cross");
+	});
 
 });
